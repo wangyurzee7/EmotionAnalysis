c4f09ad9a7fee3560a31b39fde334a22772dd5ed
wangyurzee7 <wangyz17@tsinghua.org.cn>
2019-05-31 02:21:07 +0800

    who am i

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ if __name__=='__main__':
     test_file=sys.argv[2]
     method=sys.argv[3] if len(sys.argv)>=4 else "all"
     emb_source=sys.argv[4] if len(sys.argv)>=5 else "train"
+    print(emb_source)
     
     train_doc=read_data(train_file)
     random.shuffle(train_doc)
